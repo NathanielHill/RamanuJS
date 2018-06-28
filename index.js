ed8be280cf0e20f662d2aa9677797bd489c58da1
Nathaniel Hill <nata@goguna.com>
2018-06-28 18:13:51 -0500

    adding mode

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ module.exports = {
       arr = arr.sort((a, b) => a - b)
       let i = arr.length / 2
       return i % 1 == 0 ? (arr[i -1] + arr[i]) / 2 : arr[Math.floor(i)]
+    },
+    mode: (arr) => {
+      // Returns mode. If multiple occurences at same frequency, returns smallest
+      return arr.sort((a, b) => {
+        arr.filter(x => x === b).length - arr.filter(x => x === a).length
+      }).shift()
     }
   }
 }
