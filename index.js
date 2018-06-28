4c029906d3d21c553d8b1770ac309163e65d0a9b
Nathaniel Hill <nata@goguna.com>
2018-06-28 18:13:43 -0500

    adding mean

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 module.exports = {
   arithmetic: {
+    mean: (arr) => {
+      let sum = 0
+      arr.forEach((x) => {
+        sum += x
+      })
+      return sum / arr.length
+    },
     median: (arr) => {
       arr = arr.sort((a, b) => a - b)
       let i = arr.length / 2
